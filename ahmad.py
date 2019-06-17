858342edb88af0f14560f57488d5a90ba7646945
ahmad <alivar6@gmail.com>
2019-06-17 06:40:02 +0430

    tree

--- ahmad.py
+++ ahmad.py
@@ -5,26 +5,25 @@ import numpy as np
 import mnist_reader
 import feature
 import test
-Z_train, y_train = mnist_reader.load_mnist("",'train')
-Z_test,y_test = mnist_reader.load_mnist("",'t10k')
-T_test = feature.feature(Z_test)
-X_test = np.zeros((len(Z_test),28*28+113))
-for i in range(len(X_test)):
-   X_test[i]= np.append(T_test[i],Z_test[i])
-print(X_test[0])
-b=[1 for i in range(10)]
-X_train=np.load('save_pos.npy')
+def extract_feature_vector():
+    Z_train, y_train = mnist_reader.load_mnist("", 'train')
+    Z_test, y_test = mnist_reader.load_mnist("", 't10k')
+    T_train =feature.feature(Z_train)
+    T_test = feature.feature(Z_test)
+    X_train=np.zeros((len(Z_train),28*28 +113))
+    X_test = np.zeros((len(Z_test), 28 * 28 + 113))
+    for i in range(len(X_test)):
+        X_test[i] = np.append(T_test[i], Z_test[i])
+    for i in range(len(X_train)):
+        X_train[i] = np.append(T_train[i], Z_train[i])
+
+    return X_train,y_train,X_test,y_test
+
+
+b = [1 for i in range(10)]
+X_train,y_train,X_test,y_test = extract_feature_vector()
+print(len(X_train[0]))
 w = [[1 for i in range(len(X_train[0]))] for j in range(10)]
-'''''
-for i in range(len(T_train)):
-    T_train[i]=T_train[i]*10
-X_train = np.zeros((len(Z_train),28*28+113))
-
-for i in range(len(X_train)):
-   X_train[i]= np.append(T_train[i],Z_train[i])
-'''
-print(X_train[0])
-
 for z in range(100):
     for i in range(60000):
         maxim, maxIndex = (np.dot(w[5], X_train[i])  + b[5]), 5
@@ -36,10 +35,10 @@ for z in range(100):
         if maxIndex == y_train[i]:
             continue
         else:
-            w[maxIndex] -= (X_train[i])*0.001
-            b[maxIndex] -=0.001
-            w[int(y_train[i])] += X_train[i]*0.001
-            b[int(y_train[i])] +=0.001
+            w[maxIndex] -= (X_train[i])*0.01
+            b[maxIndex] -=0.01
+            w[int(y_train[i])] += X_train[i]*0.01
+            b[int(y_train[i])] +=0.01
     print(test.test_data(w, b, X_test, y_test))
 
 print(test.test_data(w,b,X_train,y_train))
